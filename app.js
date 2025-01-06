4629e1fbe6fd043f41a3f87f84110ecf3f95a172
Ezzytechs <92188269+Ezzytechs@users.noreply.github.com>
2025-01-06 10:46:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDB = require("./config/database_config");
 const doctorRouter = require("./features/doctor/routes/doctor.route");
 const patientRouter = require("./features/patient/routes/patient.route");
+const appointmentRouter = require("./features/appointment/routes/appointment.routes");
+
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +12,7 @@ app.use(express.json());
 //endpoints
 app.use("/api/v1/doc-on-backend/doctor", doctorRouter);
 app.use("/api/v1/doc-on-backend/patient", patientRouter);
+app.use("/api/v1/doc-on-backend/appointment", appointmentRouter);
 
 const port = process.env.PORT || 5000;
 
