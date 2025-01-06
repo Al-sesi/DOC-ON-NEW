e23bbc6b0ec3cce63da80ec2569a641f9c5d6b4a
Ezzytechs <92188269+Ezzytechs@users.noreply.github.com>
2025-01-06 10:41:09 +0100

    Create appointment_model.js

---
+++ features/appointment/model/appointment_model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema({
+  date: String,
+  time: String,
+  doctor: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
+  patientId: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
+  status: { type: String, default: "available" },
+  telehealthLink: String,
+});
+
+module.exports = mongoose.model("Appointment", appointmentSchema);
+
+
