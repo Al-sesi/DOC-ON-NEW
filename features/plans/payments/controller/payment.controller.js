b12b6941b64bf1b194d6920d4780694edd52464c
Nova Technology ltd <safianusani2004@gmail.com>
2025-01-21 01:50:21 +0100

    total code update

--- features/plans/payments/controller/payment.controller.js
+++ features/plans/payments/controller/payment.controller.js
@@ -1,36 +1,37 @@
 const { v4 } = require("uuid");
-const Transactions= require("../model/payment.model");
-const {Subscription}=require("../../subscription/model/subscription_model");
-const createTransaction = require('../services/monnifyService');
-const {sendEmail}=require("../../../../config/mailer_config")
+const Transactions = require("../model/payment.model");
+const { Subscription } = require("../../subscription/model/subscription_model");
+const createTransaction = require("../services/monnifyService");
+const { sendEmail } = require("../../../../config/mailer_config");
 const initializeDoctorPayment = async (req, res) => {
   try {
-    const {planID} = req.body;
-    if (!planID) return res.status(404).json("Subscription ID must be provided");
-   const plan= await Subscription.findOne({subscriptionID:planID})
-   if (!plan) return res.status(404).json("Subscription Not Found");
-    
-    const email=req.doctor.email;
-    const subscriberDetails= req.doctor.docOnID;
-    
-  //Make sure right user purchase right plan
-  if (plan.category!=="Doctor" || !subscriberDetails || !email){
-    return res.status(404).json("This Subscription type is for Doctors only");
-  }
-    
+    const { planID } = req.body;
+    if (!planID)
+      return res.status(404).json("Subscription ID must be provided");
+    const plan = await Subscription.findOne({ subscriptionID: planID });
+    if (!plan) return res.status(404).json("Subscription Not Found");
+
+    const email = req.doctor.email;
+    const subscriberDetails = req.doctor.docOnID;
+
+    //Make sure right user purchase right plan
+    if (plan.category !== "Doctor" || !subscriberDetails || !email) {
+      return res.status(404).json("This Subscription type is for Doctors only");
+    }
+
     //Initialize payment
     const transactionDetails = {
-      amount:plan.price,
-      customerEmail:email,
+      amount: plan.price,
+      customerEmail: email,
       subscriberDetails,
-      planDetails:planID,
-      paymentReference:v4(),
+      planDetails: planID,
+      paymentReference: v4(),
       paymentDescription: `Payment for ${plan.name} subscription`,
-      currencyCode: 'NGN',
-  };
+      currencyCode: "NGN",
+    };
     const transaction = await createTransaction(transactionDetails);
 
-    res.status(200).json({email, price});
+    res.status(200).json({ email, price });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -38,31 +39,32 @@ const initializeDoctorPayment = async (req, res) => {
 
 const initializePatientPayment = async (req, res) => {
   try {
-    const {planID} = req.body;
-    
-    if (!planID) return res.status(404).json("Subscription ID must be provided");
-  const plan= await Subscription.findOne({subscriptionID:planID})
-  if (!plan) return res.status(404).json("Subscription Not Found");
-    
-    const email=req.patient.email;
-    const subscriberDetails= req.patient.docOnID;
-   
-  //Make sure right user purchase right plan
-  if (plan.category==="Doctor" || !email || !subscriberDetails){
-  return res.status(404).json("This Subscription type is for Doctors only");
-  }
+    const { planID } = req.body;
+
+    if (!planID)
+      return res.status(404).json("Subscription ID must be provided");
+    const plan = await Subscription.findOne({ subscriptionID: planID });
+    if (!plan) return res.status(404).json("Subscription Not Found");
+
+    const email = req.patient.email;
+    const subscriberDetails = req.patient.docOnID;
+
+    //Make sure right user purchase right plan
+    if (plan.category === "Doctor" || !email || !subscriberDetails) {
+      return res.status(404).json("This Subscription type is for Doctors only");
+    }
     //Initialize payment
     const transactionDetails = {
-      amount:plan.price,
-      customerEmail:email,
+      amount: plan.price,
+      customerEmail: email,
       subscriberDetails,
-      planDetails:planID,
-      paymentReference:v4(),
+      planDetails: planID,
+      paymentReference: v4(),
       paymentDescription: `Payment for ${plan.name} subscription`,
-      currencyCode: 'NGN',
-  };
+      currencyCode: "NGN",
+    };
     const transaction = await createTransaction(transactionDetails);
-    res.status(200).json({email, price});
+    res.status(200).json({ email, price });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -72,32 +74,37 @@ const verifyPayment = async (req, res) => {
   const { eventType, eventData } = req.body;
   try {
     // Handle successful transaction
-      const plan= await Subscription.findOne({subscriptionID:metadata.planDetails})
-       if (!plan) return res.status(404).json("Subscription Not Found");
-    
-    if (eventType === 'TRANSACTION_COMPLETED') {
-   const metadata= eventData.metadata;
-      
-    const transaction = new Transactions({
-      transactionID: v4(),
-      ...metadata,
-      planDetails:plan._id, //For populate methods
+    const plan = await Subscription.findOne({
+      subscriptionID: metadata.planDetails,
     });
-    await transaction.save();
-      const para=plan.category!== "Doctor" ?`book appointments with healthcare professionals.`:`recieve appointments from Patients.`;
-      const month = plan.duration > 1 ? 's' : '';
+    if (!plan) return res.status(404).json("Subscription Not Found");
+
+    if (eventType === "TRANSACTION_COMPLETED") {
+      const metadata = eventData.metadata;
+
+      const transaction = new Transactions({
+        transactionID: v4(),
+        ...metadata,
+        planDetails: plan._id, //For populate methods
+      });
+      await transaction.save();
+      const para =
+        plan.category !== "Doctor"
+          ? `book appointments with healthcare professionals.`
+          : `recieve appointments from Patients.`;
+      const month = plan.duration > 1 ? "s" : "";
 
       //Get plan expiration date
       const duration = plan.duration * 30;
       const date = new Date();
 
-const today = date.toISOString().split('T')[0];
+      const today = date.toISOString().split("T")[0];
 
-const expiration = new Date(date);
-expiration.setDate(date.getDate() + duration);
-const expirationDate= expiration.toISOString().split('T')[0];
-      
-      const successMessage=`<html>
+      const expiration = new Date(date);
+      expiration.setDate(date.getDate() + duration);
+      const expirationDate = expiration.toISOString().split("T")[0];
+
+      const successMessage = `<html>
       <head>
         <style>
           body { font-family: Arial, sans-serif; color: #333; }
@@ -134,14 +141,10 @@ const expirationDate= expiration.toISOString().split('T')[0];
       </body>
     </html>
   `;
-    sendEmail(
-      metadata.email,
-      `Subscription Successful`,
-      successMessage  
-    )
+      sendEmail(metadata.email, `Subscription Successful`, successMessage);
       res.status(200).json(transaction);
-  } else if (eventType === 'TRANSACTION_FAILED') {
-    const failedMessage=`
+    } else if (eventType === "TRANSACTION_FAILED") {
+      const failedMessage = `
     <html>
       <head>
         <style>
@@ -168,7 +171,7 @@ const expirationDate= expiration.toISOString().split('T')[0];
           </tr>
           <tr>
             <th>Date of Attempt</th>
-            <td>${new Date(Date.now()).toISOString().split('T')[0]}</td>
+            <td>${new Date(Date.now()).toISOString().split("T")[0]}</td>
           </tr>
         </table>
         <p>Possible reasons for failure include:</p>
@@ -183,14 +186,9 @@ const expirationDate= expiration.toISOString().split('T')[0];
       </body>
     </html>
   `;
-    sendEmail(
-      metadata.email,
-      `Subscription Failed`,
-      failedMessage  
-    )
-    // Handle failed transaction
-    console.log('Transaction failed:', eventData);
-  }
+      sendEmail(metadata.email, `Subscription Failed`, failedMessage);
+      console.log("Transaction failed:", eventData);
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
